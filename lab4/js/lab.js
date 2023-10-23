0a8b824e1c1160d880a101f6067c5f8a155c9b96
ashuynh6 <ashuynh65@gmail.com>
2023-10-22 23:42:20 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect Four
+// An implementation of the game
+// Ashley Huynh <asthhuyn@ucsc.edu>
+// October 23 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// initialize the grid with empty cells
+// set up two players, each with a set of pieces
+// Main game loop:
+  // display the current state of the grid
+  // ask the current player for their move and chosen column
+  // if the move is valid:
+    // place the player's token in the specified column on the board
+    // check for a win
+    // check for a tie 
+    // switch to the next player
+// display the winner or declare a draw
