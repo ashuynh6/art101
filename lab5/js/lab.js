bd0d7cffa2866dbaa777d27b53abd875a3d18910
ashuynh6 <ashuynh65@gmail.com>
2023-10-25 14:29:32 -0700

    start lab 5/mod lab 4

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,10 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+make = "Ford";
+"Ford"
+model = "Taurus";
+"Taurus"
+color = "Rusty";
+"Rusty"
+year = 1995;
+1995
+age = 2019 - year;
+24
