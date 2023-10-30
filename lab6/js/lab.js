0f5b6fdeed9e9116572a6730835dea17e3816696
ashuynh6 <ashuynh65@gmail.com>
2023-10-29 23:22:49 -0700

    mod lab 6/add lab 7

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * Ashley Huynh <asthhuyn@ucsc.edu>
+ * October 29 2023
+ */
 
-// Constants
+// myTransport Variables
+myTransport = ["legs", "bus", "car"];
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// myMainRide Object
+myMainRide = {
+        make : "Lexus",
+        model : "RC",
+        color : "atomic silver",
+        year : 2019,
+        age : function() {
+            return 2023 - this.year;
+        }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// myTransport Output
+document.writeln("Getting around: " + myTransport + "<br>");
 
-// let's get this party started
-main();
+// myMainRide Output
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
