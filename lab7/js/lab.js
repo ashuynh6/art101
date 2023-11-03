981c093e4f1881a01cba6b7ab19abd9ec6e99060
ashuynh6 <ashuynh65@gmail.com>
2023-11-02 23:16:06 -0700

    mod lab 7/update all titles/added titles in index.html

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,15 +1,23 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Ashley Huynh <asthhuyn@ucsc.edu>
+// Date: November 3 2023
 
 // Constants
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortUserName() {
+  var userName = window.prompt("Hello, what name needs repairing?");
+  console.log("userName = ", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray = ", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort = ", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted = ", nameSorted);
+  return nameSorted;
 }
 
 function main() {
@@ -19,3 +27,6 @@ function main() {
 
 // let's get this party started
 main();
+
+// Output
+document.writeln(sortUserName(), "</br>");
