6cb9329f56330ff17886bba90a32a37e875155c0
ashuynh6 <ashuynh65@gmail.com>
2023-11-06 23:22:30 -0800

    mod lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Ashley Huynh <asthhuyn@ucsc.edu>
+// Date: November 6 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function multiply(a) {
+  return a * 10;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+// test function
+console.log("4 x 10 = ", multiply(4));
+console.log("7 x 10 = ", multiply(7));
+console.log("11 x 10 = ", multiply(11));
+
+a = [4, 7, 11]
+console.log("My array", a);
+
+function add5(a) {
+  return a + 5;
 }
 
-// let's get this party started
-main();
+var mapResults = '<p> This is my array: </p>' + a + '<p>This is my array being multiplied by 10: </p>' + a.map(multiply) + '<p>This is my array added by 5: </p>' + a.map((function add5(a) {return a + 5;}));
+
+console.log("Addition of array:", mapResults);
+
+var outputEl = document.getElementById("output");
+outputEl.innerHTML = mapResults;
