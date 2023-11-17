a9c82052f9a8999252bf4acaf435b7f5072266ab
ashuynh6 <ashuynh65@gmail.com>
2023-11-16 21:46:07 -0800

    mod lab 10

--- experiment/js/lab.js
+++ experiment/js/lab.js
@@ -2,20 +2,20 @@
 // Author: Your Name
 // Date:
 
-// Constants
+// if the js is working, it will say so in console
+console.log("JS is working");
+// if still not working, it's smt else
 
-// Functions
+$(document).ready(function(){
+  // if button is activated, it will say so in console
+  console.log("Button clicked!");
+  $("#my-button").append("<button>Press Me</button>")
+  $("#my-button").click(function(){
+  var name = prompt("Name please:");
+  console.log("Prompt returned: " + name);
+  $("#title").html("Hello " + name + "!");
+})
+});
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// alt to var, you can use newText
+// as follows --> newText = "Hello " + name + "!"
--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* lab.js - This simple JavaScript/jQuery script appends new element to an output div
+ * Requirements: jQuery must be loaded for this script to work.
+ * Author : Ashley Huynh <asthhuyn@ucsc.edu>
+ * Date : November 17 2023
+ */
+// cr : Lyssa Li - shift after every message with 'direction' and 'count'
+var count = 0;
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// create helper function to generate fake dialogue
+function generateRandomText() {
+  console.log("generateRandomText working");
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  if (count % 2==0) {
+      var direction = "left";
+  } else {
+    var direction = "right";
+  }
+  // append a new div to our output div
+  $("#output").append('<div class="' + direction + ' text"><p>' + newText + '</p></div>');
+  // update count
+  count = count + 1;
+});
