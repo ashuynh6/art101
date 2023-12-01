0c79af2a17f652b3b70a5329b6c37fb4df3bb728
ashuynh6 <ashuynh65@gmail.com>
2023-12-01 02:42:48 -0800

    mod lab 5, 14, 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,21 +1,51 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* Author : Ashley Huynh <asthhuyn@ucsc.edu>
+ * Date : December 4 2023
+ */ 
 
-// Constants
+$.ajax(
+  {
+    url: "https://pokeapi.co/api/v2/berry/{id or name}/",
+    data: {
+      id:1,
+      name:"cheri",
+      growth_time:3,
+      max_harvest:5,
+      natural_gift_power:60,
+      size:20,
+      smoothness:25,
+      soil_dryness:15,
+      name:"soft",
+      url:"https://pokeapi.co/api/v2/berry-firmness/2/",
+      potency:10,
+      name:"spicy",
+      url:"https://pokeapi.co/api/v2/berry-flavor/1/",
+      name:"cheri-berry",
+      url:"https://pokeapi.co/api/v2/item/126/",
+      name:"fire",
+      url:"https://pokeapi.co/api/v2/type/10/",
+    },
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(){
+      console.log("Error:", textStatus, errorThrown);
+    }
+  }
+)
 
-// Functions
+.done (
+  function(result) {
+    console.log("we are done"),
+    console.log(result);
+    $("#output").append(result[0]).question;
+    $("#output").append(result[0]).answer;
+  }
+)
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+.fail ();
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+$("#activate").click(function(){
+  
+})
