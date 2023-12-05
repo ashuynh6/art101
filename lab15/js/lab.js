75e3f3a7a5070d816ea938d704f038f46377ab8a
ashuynh6 <ashuynh65@gmail.com>
2023-12-04 17:01:02 -0800

    mod lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,50 +2,25 @@
  * Date : December 4 2023
  */ 
 
-$.ajax(
-  {
-    url: "https://pokeapi.co/api/v2/berry/{id or name}/",
-    data: {
-      id:1,
-      name:"cheri",
-      growth_time:3,
-      max_harvest:5,
-      natural_gift_power:60,
-      size:20,
-      smoothness:25,
-      soil_dryness:15,
-      name:"soft",
-      url:"https://pokeapi.co/api/v2/berry-firmness/2/",
-      potency:10,
-      name:"spicy",
-      url:"https://pokeapi.co/api/v2/berry-flavor/1/",
-      name:"cheri-berry",
-      url:"https://pokeapi.co/api/v2/item/126/",
-      name:"fire",
-      url:"https://pokeapi.co/api/v2/type/10/",
-    },
-    method: "GET",
-    dataType: "json",
-    success: function(data) {
-      console.log(data);
-    },
-    error: function(){
-      console.log("Error:", textStatus, errorThrown);
-    }
-  }
-)
+const ENDPOINT = "https://icanhazdadjoke.com/";
 
-.done (
-  function(result) {
-    console.log("we are done"),
-    console.log(result);
-    $("#output").append(result[0]).question;
-    $("#output").append(result[0]).answer;
-  }
-)
-
-.fail ();
-
+// attach eventlistener
 $("#activate").click(function(){
-  
+  console.log("Click");
+  // ajax
+  $.ajax({
+    "url": ENDPOINT,
+    "type": "GET",
+    "dataType": "json",
+    "success": function(results){
+      console.log(results);
+      // extract results
+      jokeText = results.joke;
+      // add to output div
+      $("#output").append("<p>" + jokeText);
+    },
+    "error": function(){
+      
+    },
+  })
 })
